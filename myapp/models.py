e9a1d264e36eaae50f73b7a240fa7df55de85449
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-11-16 18:42:19 +0700

    finished

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Search(models.Model):
-    search = models.CharField(max_length=100)
+    search = models.CharField(max_length=100, null=True, blank=True, default='')
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- myapp/views.py
+++ myapp/views.py
@@ -8,6 +8,7 @@ from .models import Search
 
 
 BASE_CRAIGLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_CRAIGLIST_IMG = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 # Create your views here.
@@ -27,15 +28,33 @@ def new_search(request):
 
     soup = BeautifulSoup(data, features='html.parser')
     posts_list = soup.find_all('li', {'class': 'result-row'})
+    print(posts_list[0])
+    post_image = posts_list[0].find()
 
-    post_title = posts_list[0].find(class_='result-title').text
-    post_url = posts_list[0].find('a').get('href')
-    post_price = posts_list[0].find(class_='result-price').text
-    print(post_title)
-    print(post_url)
-    print(post_price)
+    final_post = []
+
+    for post in posts_list:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        post_price = post.find(class_='result-price')
+
+        if post_price.text != '$0':
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_CRAIGLIST_IMG.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_post.append((post_title, post_url, post_price, post_image_url))
+
 
     context = {
-        'search': search
+        'search': search,
+        'final_post': final_post
     }
     return render(request, 'myapp/index.html', context)
